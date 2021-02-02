eda7741a674f621e78cf90c76ac92efa540e386a
Talha Emin Aydemir <me@talhaeminaydemir.com>
2021-02-02 06:26:55 +0300

    exports added

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import {
+  Get,
+  Post,
+  Put,
+  Patch,
+  Delete,
+} from './http-methods';
+
+import Controller from './controller';
+import ControllerAbstract from './controller.abstract';
+import IController from './controller.interface';
+
+import Validation from './validation';
+
+
+export {
+  Get,
+  Post,
+  Put,
+  Patch,
+  Delete,
+  Controller,
+  ControllerAbstract,
+  IController,
+  Validation,
+};
